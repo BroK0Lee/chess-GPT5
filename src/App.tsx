c30c7b28d01b4b334ca1b2a498ac2be2a6b87ec7
BroK0Lee <loiseleur.sandy@gmail.com>
2025-08-13 22:36:12 +0200

    Fix Chess3D file extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import ChessApp from "./ChessApp"; // 2D
-import Chess3D from "./Chess3D";      // 3D
+import Chess3D from "./Chess3D.tsx";  // 3D
 
 export default function App() {
   return <Chess3D />;
